fbd61603f81513ebad8087c8b74faa93bcabe3a4
damarte <damarte86@gmail.com>
2014-10-07 15:49:07 +0200

    Arreglada orientación en iOS8

--- SIAlertView/SIAlertView.m
+++ SIAlertView/SIAlertView.m
@@ -299,7 +299,7 @@ static SIAlertView *__si_alert_current_view;
 + (void)showBackground
 {
     if (!__si_alert_background_window) {
-        __si_alert_background_window = [[SIAlertBackgroundWindow alloc] initWithFrame:[UIScreen mainScreen].bounds
+        __si_alert_background_window = [[SIAlertBackgroundWindow alloc] initWithFrame:CGRectMake([UIScreen mainScreen].bounds.origin.x, [UIScreen mainScreen].bounds.origin.y, [UIScreen mainScreen].bounds.size.height, [UIScreen mainScreen].bounds.size.width)
                                                                              andStyle:[SIAlertView currentAlertView].backgroundStyle];
         [__si_alert_background_window makeKeyAndVisible];
         __si_alert_background_window.alpha = 0;
